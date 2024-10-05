8fee4581e88cf44d88961c7a91970172a1c0a322
cjhair <kristianlcj@outlook.com>
2024-10-05 10:01:41 -0500

    ingress port 22 and 80

--- vpc.tf
+++ vpc.tf
@@ -103,8 +103,15 @@ resource "aws_security_group" "allow_ftp" {
   vpc_id      = aws_vpc.cloud2_vpc_terraform.id
 
   ingress {
-    from_port   = 20
-    to_port     = 20
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
